e1a45c617821138ace1a3c31f0740d0b666bf1e3
徐鹏飞 <250881478@qq.com>
2022-07-18 00:29:21 +0800

    1. 修复mysql5.x和mysql8.x无法互相切换的问题 2. 修复PHP扩展安装时和选择的PHP版本无法对应的问题 3. 修复Host列表操作菜单遮挡的问题 4. 新增PHP扩展ssh2

--- configs/electron-builder.ts
+++ configs/electron-builder.ts
@@ -3,7 +3,7 @@ import PublishConfig from './publish'
 
 const conf: Configuration = {
   productName: 'PhpWebStudy',
-  buildVersion: '1.0.22',
+  buildVersion: '1.0.23',
   electronVersion: '16.0.7',
   appId: 'phpstudy.xpfme.com',
   asar: true,
