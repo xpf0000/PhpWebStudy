23daf3ffc8491c5c87517754693ea83b6a758c4e
x <250881478@qq.com>
2023-02-20 09:25:16 +0800

    1. 新增i18n多语言 目前支持中英文切换 2. host站点可以直接点击打开 3. host站点编辑时现在使用增量更新, 用户自定义的内容不会再被覆盖了 4. 所有log日志改成使用monaco-editor打开, 更方便查看查找 5. 优化打包文件大小, 目前安装包大小在90+M 6. 修复php xdebug扩展无法安装的问题

--- configs/electron-builder.ts
+++ configs/electron-builder.ts
@@ -3,7 +3,7 @@ import PublishConfig from './publish'
 
 const conf: Configuration = {
   productName: 'PhpWebStudy',
-  buildVersion: '1.0.36',
+  buildVersion: '1.0.37',
   electronVersion: '16.0.7',
   appId: 'phpstudy.xpfme.com',
   asar: true,
