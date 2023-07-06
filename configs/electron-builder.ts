f0e417cd77a0bb12494bacb39aa3a8b1b8d42187
x <250881478@qq.com>
2023-07-06 22:32:52 +0800

    1. 设置新增自动升级检测开关, 应用自动升级检测可以关闭了 2. 站点列表优化, 多级子域名可以折叠收起 3. 站点新增备注, 可以设置站点备注了 4. 站点列表新增展示站点使用的php版本 5. 修复brew更新软件后, phpwebstudy需要重新选择版本的问题

--- configs/electron-builder.ts
+++ configs/electron-builder.ts
@@ -3,7 +3,7 @@ import PublishConfig from './publish'
 
 const conf: Configuration = {
   productName: 'PhpWebStudy',
-  buildVersion: '1.0.44',
+  buildVersion: '1.0.45',
   electronVersion: '23.1.1',
   appId: 'phpstudy.xpfme.com',
   asar: true,
