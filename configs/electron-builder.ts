66c3e4da4d048517b1b4a332e46efd4193fb6a6f
徐鹏飞 <250881478@qq.com>
2023-09-18 10:43:25 +0800

    1. Fixed PHP Redis Extensions install issues

--- configs/electron-builder.ts
+++ configs/electron-builder.ts
@@ -3,7 +3,7 @@ import PublishConfig from './publish'
 
 const conf: Configuration = {
   productName: 'PhpWebStudy',
-  buildVersion: '1.0.50',
+  buildVersion: '1.0.51',
   electronVersion: '23.1.1',
   appId: 'phpstudy.xpfme.com',
   asar: true,
