bc783ff4ae58dbec3440996122a45e86a7310995
徐鹏飞 <250881478@qq.com>
2023-09-18 11:06:39 +0800

    1. Fixed PHP Redis Extensions install issues

--- static/fork/node.js
+++ static/fork/node.js
@@ -78,7 +78,8 @@ class Manager extends BaseManager {
   }
 
   installNvm() {
-    exec('curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash')
+    const env = this._fixEnv()
+    exec('curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash', env)
       .then(this._thenSuccess)
       .catch(this._catchError)
   }
