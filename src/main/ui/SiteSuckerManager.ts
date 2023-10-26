e8ac8cb74864538096937a4de9fa326de1aa6c0a
x <250881478@qq.com>
2023-10-27 00:44:06 +0800

    add site sucker

--- src/main/ui/SiteSuckerManager.ts
+++ src/main/ui/SiteSuckerManager.ts
@@ -4,6 +4,9 @@ import { basename, dirname, extname, join } from 'path'
 import { createWriteStream, mkdirpSync, readFile, writeFile, existsSync } from 'fs-extra'
 import request from '@shared/request'
 import { md5 } from '@shared/utils'
+const os = require('os')
+
+const CPU_Count = os.cpus().length
 
 const ExcludeHost = [
   'www.google-analytics.com',
@@ -14,39 +17,31 @@ const ExcludeHost = [
   'www.google.com'
 ]
 
+type LinkState = 'wait' | 'running' | 'success' | 'fail' | 'replace'
+
 type PageLink = {
   url: string
   raw: string
   replaceUrl: string
   fromPage: string
   saveFile: string
-  loaded: boolean
+  state: LinkState
 }
 
 class SiteSuckerManager {
   window?: BrowserWindow
   pages: Array<PageLink> = []
-  pageLoaded: Set<string> = new Set()
   pageLinks: Array<PageLink> = []
   currentPage?: PageLink
 
-  req: Set<string> = new Set()
-
-  handled: Set<string> = new Set()
-  htmls: Set<string> = new Set()
-  links: { [k: string]: string } = {}
   timer?: NodeJS.Timeout
   baseHost?: string
   baseDir?: string
   running = false
-  urls: Array<string> = []
-  loadUrls: Set<string> = new Set()
-  replacePageUrl: { [k: string]: string } = {}
-  replaceLinkUrl: { [k: string]: string } = {}
 
-  constructor() {
-    this.down = this.down.bind(this)
-  }
+  task: Array<number> = []
+
+  constructor() {}
 
   async show(url: string) {
     this.destory()
@@ -57,13 +52,13 @@ class SiteSuckerManager {
     this.baseDir = join('/Users/x/Desktop/AAA/', urlObj.host)
 
     const saveFile = this.urlToDir(url, true)
-    const currentPage = {
+    const currentPage: PageLink = {
       url,
       raw: '',
       replaceUrl: '',
       fromPage: '',
       saveFile,
-      loaded: false
+      state: 'wait'
     }
     this.pages.push(currentPage)
 
@@ -84,17 +79,12 @@ class SiteSuckerManager {
     this.window.webContents.session.webRequest.onBeforeRequest((details, callback) => {
       const u = new URL(details.url)
       if (ExcludeHost.includes(u.host)) {
-        console.log('onBeforeRequest ExcludeHost: ', details.url)
         callback({
           cancel: true
         })
         return
       }
-      this.req.add(details.url)
       callback({})
-    })
-    this.window.webContents.session.webRequest.onCompleted((details) => {
-      this.req.delete(details.url)
     })
     this.window.webContents.session.webRequest.onHeadersReceived((details, callback) => {
       if (
@@ -117,13 +107,13 @@ class SiteSuckerManager {
           if (uobj.host === this.baseHost) {
             if (!this.pages.find((p) => p.url === url)) {
               const saveFile = this.urlToDir(url, true)
-              const item = {
+              const item: PageLink = {
                 url,
                 raw: '',
                 replaceUrl: '',
                 fromPage: '',
                 saveFile,
-                loaded: false
+                state: 'wait'
               }
               this.pages.push(item)
               this.fetchPage().then()
@@ -136,57 +126,239 @@ class SiteSuckerManager {
     this.window.on('close', () => {
       this.destory()
     })
+    this.running = false
     this.fetchPage().then()
   }
 
-  async replaceContent(link: PageLink) {
-    if (link.fromPage && existsSync(link.fromPage) && link.raw !== link.replaceUrl) {
-      let content = await readFile(link.fromPage, 'utf8')
-      content = content.replace(new RegExp(`href="${link.raw}"`, 'g'), `href="${link.replaceUrl}"`)
-      await writeFile(link.fromPage, content)
+  onPageLoaded() {
+    const parseHtmlPage = (html: string) => {
+      const reg = new RegExp('<a([^<]+)href="([^"]+)"', 'g')
+      let result
+      const imgs = []
+      while ((result = reg.exec(html)) != null) {
+        imgs.push(result[2].trim())
+      }
+      const alinks = imgs.filter((u) => {
+        if (u.startsWith('#')) {
+          return false
+        }
+        let right = true
+        try {
+          const urlObj = new URL(u, this.currentPage?.url)
+          if (urlObj.host !== this.baseHost || !urlObj.protocol.includes('http')) {
+            right = false
+          }
+        } catch (e) {
+          right = false
+        }
+        return right
+      })
+      const linkUrls: Array<PageLink> = alinks.map((a) => {
+        const u = new URL(a, this.currentPage?.url)
+        u.hash = ''
+        const linkUrl = u.toString()
+        const saveFile = this.urlToDir(linkUrl, true)
+        const replaceUrl = saveFile.replace(this.baseDir!, '')
+        return {
+          url: linkUrl,
+          raw: a,
+          replaceUrl,
+          fromPage: this.currentPage?.saveFile ?? '',
+          saveFile,
+          state: 'wait'
+        }
+      })
+      this.pages.push(...linkUrls)
     }
+    const parseHtmlLink = (html: string) => {
+      const links = []
+      let result
+      let reg = new RegExp('<link([^<]+)href="([^"]+)"', 'g')
+      while ((result = reg.exec(html)) != null) {
+        links.push(result[2].trim())
+      }
+      reg = new RegExp('<script([^<]+)src="([^"]+)"', 'g')
+      while ((result = reg.exec(html)) != null) {
+        links.push(result[2].trim())
+      }
+      reg = new RegExp('<img([^<]+)src="([^"]+)"', 'g')
+      while ((result = reg.exec(html)) != null) {
+        links.push(result[2].trim())
+      }
+      reg = new RegExp('<video([^<]+)src="([^"]+)"', 'g')
+      while ((result = reg.exec(html)) != null) {
+        links.push(result[2].trim())
+      }
+      reg = new RegExp('<video([^<]+)poster="([^"]+)"', 'g')
+      while ((result = reg.exec(html)) != null) {
+        links.push(result[2].trim())
+      }
+      reg = new RegExp('<source([^<]+)src="([^"]+)"', 'g')
+      while ((result = reg.exec(html)) != null) {
+        links.push(result[2].trim())
+      }
+      const linkUrls: Array<PageLink> = links
+        .filter((a) => {
+          const u = new URL(a, this.currentPage?.url)
+          return !ExcludeHost.includes(u.host) && u.protocol.includes('http')
+        })
+        .map((a) => {
+          const u = new URL(a, this.currentPage?.url)
+          u.hash = ''
+          const linkUrl = u.toString()
+          const saveFile = this.urlToDir(linkUrl)
+          const replaceUrl = saveFile.replace(this.baseDir!, '')
+          return {
+            url: linkUrl,
+            raw: a,
+            replaceUrl,
+            fromPage: this.currentPage?.url ?? '',
+            saveFile,
+            state: 'wait'
+          }
+        })
+      this.pageLinks.push(...linkUrls)
+    }
+    return new Promise((resolve) => {
+      console.time('onPageLoaded')
+      this.window?.webContents
+        ?.executeJavaScript('document.documentElement.outerHTML', true)
+        .then(async (html) => {
+          const saveFile = this.currentPage!.saveFile
+          const dir = dirname(saveFile)
+          mkdirpSync(dir)
+          await writeFile(saveFile, html)
+          parseHtmlPage(html)
+          parseHtmlLink(html)
+          console.timeEnd('onPageLoaded')
+          resolve(true)
+        })
+        .catch(() => {
+          this.currentPage!.state = 'fail'
+          console.timeEnd('onPageLoaded')
+          resolve(true)
+        })
+    })
   }
 
   async fetchPage() {
     if (this.running) {
       return
     }
-    const page = this.pages.find((p) => !p.loaded)
+    const page = this.pages.find((p) => p.state === 'wait')
     if (!page) {
       this.running = false
       console.log('页面获取完毕 !!!')
-      console.log('this.pages: ', this.pages)
-      console.log('this.pageLinks: ', this.pageLinks)
+      console.log(
+        'this.pages: ',
+        this.pages.filter((p) => p.state !== 'success')
+      )
+      console.log(
+        'this.pageLinks: ',
+        this.pageLinks.filter((p) => p.state !== 'success')
+      )
       return
     }
     this.running = true
-    const pageLoaded = this.pages.find((p) => p.loaded && p.url === page.url)
+    const pageLoaded = this.pages.find(
+      (p) => (p.state === 'replace' || p.state === 'success') && p.url === page.url
+    )
     if (pageLoaded) {
-      page.loaded = true
-      await this.replaceContent(page)
+      page.state = 'replace'
+      this.running = false
       await this.fetchPage()
       return
     }
     this.currentPage = page
+    this.currentPage.state = 'running'
     const wait = () => {
       this.timer = setTimeout(async () => {
         this.timer = undefined
         await this.onPageLoaded()
+        this.currentPage!.state = 'replace'
+        this.running = false
+        await this.fetchLink()
+        console.log('this.currentPage: ', this.currentPage)
         await this.fetchPage()
       }, 2000)
     }
     console.time('loadURL')
     this.window!.loadURL(page.url)
       .then(() => {
-        console.log('loadURL success: ', page.url)
         console.timeEnd('loadURL')
         wait()
       })
-      .catch((err) => {
-        console.log('loadURL fail: ', err)
+      .catch(() => {
         console.timeEnd('loadURL')
         wait()
       })
+  }
+
+  async fetchLink() {
+    const now = this.task.length
+    for (let i = now; i < CPU_Count; i += 1) {
+      const link = this.pageLinks.find((l) => l.state === 'wait')
+      if (link) {
+        link.state = 'running'
+        this.task.push(0)
+        const linkLoaded = this.pageLinks.find(
+          (p) => (p.state === 'replace' || p.state === 'success') && p.url === link.url
+        )
+        if (linkLoaded) {
+          link.state = 'replace'
+          this.task.pop()
+          this.fetchLink().then()
+        } else {
+          const saveFile = link.saveFile
+          const dir = dirname(saveFile)
+          mkdirpSync(dir)
+          const stream = createWriteStream(saveFile)
+          stream.on('finish', () => {
+            stream.close(() => {
+              link.state = 'replace'
+              this.task.pop()
+              this.fetchLink().then()
+            })
+          })
+          stream.on('error', () => {
+            stream.close(() => {
+              link.state = 'fail'
+              this.task.pop()
+              this.fetchLink().then()
+            })
+          })
+          this.window!.webContents.session.cookies.get({ url: this.currentPage!.url }).then(
+            (cookies) => {
+              const cookieArr: Array<string> = []
+              cookies.forEach((cookie) => {
+                cookieArr.push(`${cookie.name}=${cookie.value};`)
+              })
+              request({
+                url: link.url,
+                method: 'get',
+                responseType: 'stream',
+                headers: {
+                  cookie: cookieArr.join(' ')
+                }
+              })
+                .then((res) => {
+                  res.data.pipe(stream)
+                })
+                .catch(() => {
+                  stream.close(() => {
+                    link.state = 'fail'
+                    this.task.pop()
+                    this.fetchLink().then()
+                  })
+                })
+            }
+          )
+        }
+      } else {
+        await this.doReplace()
+        break
+      }
+    }
   }
 
   urlToDir(url: string, isPageUrl?: boolean) {
@@ -241,233 +413,39 @@ class SiteSuckerManager {
     return saveFile
   }
 
-  onPageLoaded() {
-    const parseHtmlPage = (html: string) => {
-      const reg = new RegExp('<a([^<]+)href="([^"]+)"', 'g')
-      let result
-      const imgs = []
-      while ((result = reg.exec(html)) != null) {
-        imgs.push(result[2].trim())
+  async doReplace() {
+    for (const page of this.pages) {
+      if (page.state === 'replace') {
+        await this.replaceContent(page)
+        page.state = 'success'
       }
-      const alinks = imgs.filter((u) => {
-        if (u.startsWith('#')) {
-          return false
-        }
-        let right = true
-        try {
-          const urlObj = new URL(u, this.currentPage?.url)
-          if (urlObj.host !== this.baseHost || !urlObj.protocol.includes('http')) {
-            right = false
+      if (page.state === 'success') {
+        const needReplaceLinks = this.pageLinks.filter(
+          (p) => p.state === 'replace' && p.fromPage === page.url
+        )
+        if (needReplaceLinks.length > 0) {
+          const saveFile = page.saveFile
+          if (existsSync(saveFile)) {
+            let content = await readFile(saveFile, 'utf-8')
+            needReplaceLinks.forEach((link) => {
+              if (link.raw !== link.replaceUrl) {
+                content = content.replace(new RegExp(link.raw, 'g'), link.replaceUrl)
+              }
+              link.state = 'success'
+            })
+            await writeFile(saveFile, content)
           }
-        } catch (e) {
-          right = false
         }
-        return right
-      })
-      const linkUrls = alinks.map((a) => {
-        const u = new URL(a, this.currentPage?.url)
-        u.hash = ''
-        const linkUrl = u.toString()
-        const saveFile = this.urlToDir(linkUrl, true)
-        const replaceUrl = saveFile.replace(this.baseDir!, '')
-        return {
-          url: linkUrl,
-          raw: a,
-          replaceUrl,
-          fromPage: this.currentPage?.saveFile ?? '',
-          saveFile,
-          loaded: false
-        }
-      })
-      this.pages.push(...linkUrls)
+      }
     }
-    const parseHtmlLink = (html: string) => {
-      const links = []
-      let result
-      let reg = new RegExp('<link([^<]+)href="([^"]+)"', 'g')
-      while ((result = reg.exec(html)) != null) {
-        links.push(result[2].trim())
-      }
-      reg = new RegExp('<script([^<]+)src="([^"]+)"', 'g')
-      while ((result = reg.exec(html)) != null) {
-        links.push(result[2].trim())
-      }
-      reg = new RegExp('<img([^<]+)src="([^"]+)"', 'g')
-      while ((result = reg.exec(html)) != null) {
-        links.push(result[2].trim())
-      }
-      reg = new RegExp('<video([^<]+)src="([^"]+)"', 'g')
-      while ((result = reg.exec(html)) != null) {
-        links.push(result[2].trim())
-      }
-      reg = new RegExp('<video([^<]+)poster="([^"]+)"', 'g')
-      while ((result = reg.exec(html)) != null) {
-        links.push(result[2].trim())
-      }
-      reg = new RegExp('<source([^<]+)src="([^"]+)"', 'g')
-      while ((result = reg.exec(html)) != null) {
-        links.push(result[2].trim())
-      }
-      const linkUrls = links
-        .filter((a) => {
-          const u = new URL(a, this.currentPage?.url)
-          const isExclude = ExcludeHost.includes(u.host)
-          return (
-            !isExclude &&
-            (a.startsWith('https://') || a.startsWith('http://') || a.startsWith('//'))
-          )
-        })
-        .map((a) => {
-          const u = new URL(a, this.currentPage?.url)
-          u.hash = ''
-          const linkUrl = u.toString()
-          const saveFile = this.urlToDir(linkUrl)
-          const replaceUrl = saveFile.replace(this.baseDir!, '')
-          return {
-            url: linkUrl,
-            raw: a,
-            replaceUrl,
-            fromPage: this.currentPage?.url ?? '',
-            saveFile,
-            loaded: false
-          }
-        })
-      this.pageLinks.push(...linkUrls)
-    }
-    return new Promise((resolve) => {
-      console.log('onPageLoaded Start !!!', Array.from(this.req))
-      console.time('onPageLoaded')
-      this.window?.webContents
-        ?.executeJavaScript('document.documentElement.outerHTML', true)
-        .then(async (html) => {
-          const saveFile = this.currentPage!.saveFile
-          const dir = dirname(saveFile)
-          mkdirpSync(dir)
-          await writeFile(saveFile, html)
-          parseHtmlPage(html)
-          parseHtmlLink(html)
-          this.currentPage!.loaded = true
-          console.log('onPageLoaded END !!!')
-          console.timeEnd('onPageLoaded')
-          resolve(true)
-        })
-    })
   }
 
-  down() {
-    if (this.running) {
-      return
+  async replaceContent(page: PageLink) {
+    if (page.fromPage && existsSync(page.fromPage) && page.raw !== page.replaceUrl) {
+      let content = await readFile(page.fromPage, 'utf8')
+      content = content.replace(new RegExp(`href="${page.raw}"`, 'g'), `href="${page.replaceUrl}"`)
+      await writeFile(page.fromPage, content)
     }
-    let url = ''
-    for (const u in this.links) {
-      if (!this.links[u]) {
-        url = u
-        break
-      }
-    }
-    if (!url) {
-      console.log('END !!!', this.urls)
-      const pageURL = this.urls.pop()
-      if (pageURL && !this.loadUrls.has(pageURL)) {
-        this.loadUrls.add(pageURL)
-        console.log('load url: ', pageURL)
-        this?.window?.loadURL(pageURL)?.then(this.down)?.catch(this.down)
-      } else {
-        const data = {
-          page: Array.from(this.loadUrls),
-          links: this.links
-        }
-        writeFile(
-          join('/Users/x/Desktop/AAA/', `${this.baseHost}.json`),
-          JSON.stringify(data)
-        ).then()
-        this.timer = setTimeout(() => {
-          this.down()
-        }, 1000)
-      }
-      return
-    }
-    this.running = true
-    console.log('down: ', url)
-    let saveFile = ''
-    if (url.includes(this.baseHost!)) {
-      let pathDir = url.split(`${this.baseHost!}`).pop() ?? ''
-      if (pathDir.startsWith('/')) {
-        pathDir = pathDir.replace('/', '')
-      }
-      if (this.htmls.has(url)) {
-        const ext = extname(pathDir)
-        if (!!ext) {
-          pathDir = pathDir.replace(ext, '.html')
-        } else {
-          pathDir += '.html'
-        }
-      }
-      pathDir = pathDir.trim()
-      if (pathDir === '.html') {
-        pathDir = 'index.html'
-      }
-      saveFile = join(this.baseDir!, pathDir)
-    } else {
-      const ext = extname(url.split('/').pop()!)
-      saveFile = join(this.baseDir!, `outsite/${md5(url)}${ext}`)
-    }
-    console.log('saveFile: ', saveFile)
-    const dir = dirname(saveFile)
-    mkdirpSync(dir)
-    const stream = createWriteStream(saveFile)
-    stream.on('finish', async () => {
-      this.links[url] = saveFile
-      const doReplace = async () => {
-        const replaceUrl = saveFile.replace(this.baseDir!, '')
-        for (const u in this.links) {
-          if (this.links[u] && this.links[u] !== 'ERROR') {
-            const f = this.links[u]
-            const ext = extname(f)
-            if (ext === '.html' || ext === '.css' || ext === '.js') {
-              let content = await readFile(f, 'utf8')
-              content = content.replace(new RegExp(url, 'g'), replaceUrl)
-              await writeFile(f, content)
-            }
-          }
-        }
-      }
-      stream.close(async () => {
-        await doReplace()
-        // await parseHtml()
-        this.running = false
-        this.down()
-      })
-    })
-    stream.on('error', (err) => {
-      console.log('stream error: ', err)
-      this.links[url] = 'ERROR'
-      this.running = false
-      this.down()
-    })
-    this.window!.webContents.session.cookies.get({ url }).then((cookies) => {
-      const cookieArr: Array<string> = []
-      cookies.forEach((cookie) => {
-        cookieArr.push(`${cookie.name}=${cookie.value};`)
-      })
-      request({
-        url,
-        method: 'get',
-        responseType: 'stream',
-        timeout: 5000,
-        headers: {
-          cookie: cookieArr.join(' ')
-        }
-      })
-        .then((res) => {
-          res.data.pipe(stream)
-        })
-        .catch(() => {
-          this.links[url] = 'ERROR'
-          this.running = false
-          this.down()
-        })
-    })
   }
 
   destory() {
@@ -477,7 +455,6 @@ class SiteSuckerManager {
     }
     this.pages.splice(0)
     this.pageLinks.splice(0)
-    this.pageLoaded.clear()
     if (this.window) {
       this.window?.destroy()
     }
