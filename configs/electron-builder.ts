2f99a7e2afa1c746e87279237a545202625a5d1d
徐鹏飞 <250881478@qq.com>
2023-11-10 17:17:08 +0800

    1. host can create static sites 2. Fix the problem of nginx startup error when php version is less than 5.6 or more than 8.2. 3. Update DNS Server view, now show all server handled host

--- configs/electron-builder.ts
+++ configs/electron-builder.ts
@@ -3,7 +3,7 @@ import PublishConfig from './publish'
 
 const conf: Configuration = {
   productName: 'PhpWebStudy',
-  buildVersion: '1.1.0',
+  buildVersion: '1.2.0',
   electronVersion: '23.1.1',
   appId: 'phpstudy.xpfme.com',
   asar: true,
