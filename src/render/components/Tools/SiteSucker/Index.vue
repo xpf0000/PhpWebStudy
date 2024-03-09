897099b3e57ccb3787f97f93f6131c2fedac380d
徐鹏飞 <250881478@qq.com>
2024-03-09 17:38:03 +0800

    1. Fix bugs

--- src/render/components/Tools/SiteSucker/Index.vue
+++ src/render/components/Tools/SiteSucker/Index.vue
@@ -31,18 +31,36 @@
           </el-button-group>
         </div>
         <div class="table-wapper">
-          <el-auto-resizer>
-            <template #default="{ height, width }">
-              <el-table-v2
-                :row-height="42"
-                :columns="columns"
-                :data="links"
-                :width="width"
-                :height="height"
-                fixed
-              />
-            </template>
-          </el-auto-resizer>
+          <div class="url-block">
+            <el-auto-resizer>
+              <template #default="{ height, width }">
+                <el-table-v2
+                  :row-height="55"
+                  :header-height="55"
+                  :columns="columns"
+                  :data="links"
+                  :width="width"
+                  :height="height"
+                  fixed
+                />
+              </template>
+            </el-auto-resizer>
+          </div>
+          <div class="host-block">
+            <el-auto-resizer>
+              <template #default="{ height, width }">
+                <el-table-v2
+                  :row-height="55"
+                  :header-height="55"
+                  :columns="columnsHost"
+                  :data="hosts"
+                  :width="width"
+                  :height="height"
+                  fixed
+                />
+              </template>
+            </el-auto-resizer>
+          </div>
         </div>
       </div>
     </div>
@@ -57,23 +75,60 @@
   import { ElIcon } from 'element-plus'
   import type { Column } from 'element-plus'
   import { AsyncComponentShow } from '@/util/AsyncComponent'
+  import { ElInput } from 'element-plus'
 
   const emit = defineEmits(['doClose'])
   const siteStore = SiteSuckerStore()
   siteStore.initSetup()
   const links = computed(() => {
     const fails = siteStore.links.filter((f) => f.state === 'fail')
-    const other = siteStore.links.filter((f) => f.state !== 'fail')
-    return [...fails, ...other]
+    const wait = siteStore.links.filter((f) => f.state === 'wait')
+    const running = siteStore.links.filter((f) => f.state === 'running')
+    const other = siteStore.links.filter((f) => !['fail', 'wait', 'running'].includes(f.state))
+    return [...fails, ...wait, ...running, ...other]
+  })
+
+  const hosts = computed(() => {
+    const host = new Set()
+    siteStore.links.forEach((s) => {
+      const url = new URL(s.url)
+      host.add(url.host)
+    })
+    return Array.from(host).map((h) => {
+      return {
+        host: h
+      }
+    })
   })
 
   const url = ref('')
+  const search = ref('')
+  const searchHost = ref('')
 
   const task = computed(() => {
     return siteStore.task
   })
 
   url.value = task.value.url
+
+  const excludeLink = computed(() => {
+    return (
+      siteStore?.commonSetup?.excludeLink
+        ?.split('\n')
+        ?.map((s) => s.trim())
+        .filter((s) => !!s) ?? []
+    )
+  })
+
+  const allowChange = (host: string) => {
+    const ex = new Set(excludeLink.value)
+    if (ex.has(host)) {
+      ex.delete(host)
+    } else {
+      ex.add(host)
+    }
+    siteStore.commonSetup.excludeLink = Array.from(ex).join('\n')
+  }
 
   const columns: Column<any>[] = [
     {
@@ -83,16 +138,23 @@
       width: 150,
       class: 'url-column',
       headerClass: 'url-column',
-      cellRenderer: ({ cellData: url }) => <span class="flex items-center">{url}</span>,
+      cellRenderer: ({ cellData: url }) => (
+        <span class="flex items-center" style="padding-left: 24px">
+          {url}
+        </span>
+      ),
       headerCellRenderer: () => {
         const count = links.value.length
         const success = links.value.filter(
           (f) => f.state === 'replace' || f.state === 'success'
         ).length
         return (
-          <span class="flex items-center">
-            url({success}/{count})
-          </span>
+          <div class="wapper">
+            <span class="flex items-center">
+              url({success}/{count})
+            </span>
+            <ElInput v-model={search.value} placeholder="search"></ElInput>
+          </div>
         )
       }
     },
@@ -100,7 +162,7 @@
       key: 'state',
       title: 'state',
       dataKey: 'state',
-      width: 100,
+      width: 140,
       align: 'center',
       cellRenderer: ({ cellData: state }) => {
         if (state === 'fail') {
@@ -122,6 +184,54 @@
             </ElIcon>
           )
         }
+      }
+    }
+  ]
+
+  const columnsHost: Column<any>[] = [
+    {
+      key: 'host',
+      title: 'host',
+      dataKey: 'host',
+      width: 150,
+      class: 'url-column',
+      headerClass: 'url-column',
+      cellRenderer: ({ cellData: host }) => {
+        const filter = links.value.filter((u) => {
+          const url = new URL(u.url)
+          return url.host === host
+        })
+        const count = filter.length
+        const success = filter.filter((f) => f.state === 'replace' || f.state === 'success').length
+        return (
+          <span class="flex items-center" style="padding-left: 24px">
+            {host} ({success}/{count})
+          </span>
+        )
+      },
+      headerCellRenderer: () => {
+        return (
+          <div class="wapper">
+            <span class="flex items-center">host</span>
+            <ElInput v-model={searchHost.value} placeholder="search"></ElInput>
+          </div>
+        )
+      }
+    },
+    {
+      key: 'allow',
+      title: 'allow',
+      dataKey: 'host',
+      width: 140,
+      class: 'host-allow',
+      align: 'center',
+      cellRenderer: ({ cellData: host }) => {
+        const c = excludeLink.value.includes(host) ? 'not-allow' : 'allow'
+        return (
+          <ElIcon class={c} onClick={() => allowChange(host)}>
+            <Check />
+          </ElIcon>
+        )
       }
     }
   ]
