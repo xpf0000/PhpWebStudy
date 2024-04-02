dc5894fe738fa3a8b18c5bed84c53460ff9544c8
徐鹏飞 <250881478@qq.com>
2024-04-02 11:50:15 +0800

    1. Fix bugs

--- configs/electron-builder.ts
+++ configs/electron-builder.ts
@@ -3,7 +3,7 @@ import PublishConfig from './publish'
 
 const conf: Configuration = {
   productName: 'PhpWebStudy',
-  buildVersion: '2.4.1',
+  buildVersion: '2.4.2',
   electronVersion: '23.1.1',
   appId: 'phpstudy.xpfme.com',
   asar: true,
