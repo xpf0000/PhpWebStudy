81afffa487df2765b147e571130880fba4cb3c8a
xupengfei <250881478@qq.com>
2024-07-29 10:33:53 +0800

    init

--- src/fork/module/Node.ts
+++ src/fork/module/Node.ts
@@ -1,9 +1,8 @@
 import { Base } from './Base'
-import { execPromise, fixEnv, spawnPromise } from '../Fn'
+import { execPromise, spawnPromise } from '../Fn'
 import { ForkPromise } from '@shared/ForkPromise'
 import { join } from 'path'
 import { compareVersions } from 'compare-versions'
-import { exec } from 'child_process'
 import { existsSync } from 'fs'
 import { chmod, copyFile, unlink, appendFile } from 'fs-extra'
 
@@ -12,35 +11,39 @@ class Manager extends Base {
     super()
   }
 
-  allVersion(tool: 'fnm' | 'nvm') {
-    return new ForkPromise((resolve, reject) => {
-      let command = ''
-      if (tool === 'fnm') {
-        command = 'fnm ls-remote'
-      } else {
-        command =
-          'export NVM_DIR="${HOME}/.nvm";[ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh";nvm ls-remote'
-      }
+  _init_sh() {
+    return new ForkPromise(async (resolve, reject) => {
       try {
-        exec(
-          command,
-          {
-            env: fixEnv()
-          },
-          (error, stdout, stderr) => {
-            console.log('stdout: ', stdout)
-            console.log('stderr: ', stderr)
-            const str = stdout ?? ''
-            const all =
-              str?.match(/\sv\d+(\.\d+){1,4}\s/g)?.map((v) => {
-                return v.trim().replace('v', '')
-              }) ?? []
-            resolve({
-              all: all.reverse(),
-              tool
-            })
-          }
-        )
+        const sh = join(global.Server.Static!, 'sh/node.sh')
+        const copyfile = join(global.Server.Cache!, 'node.sh')
+        if (existsSync(copyfile)) {
+          await unlink(copyfile)
+        }
+        await copyFile(sh, copyfile)
+        await execPromise(`echo "${global.Server.Password}" | sudo -S chmod 777 ${copyfile}`)
+        resolve(copyfile)
+      } catch (e) {
+        await appendFile(join(global.Server.BaseDir!, 'debug.log'), `[Node][nvmDir][Error]: ${e}`)
+        reject(e)
+      }
+    })
+  }
+
+  allVersion(tool: 'fnm' | 'nvm') {
+    return new ForkPromise(async (resolve, reject) => {
+      try {
+        const copyfile = await this._init_sh()
+        const flag = tool === 'fnm' ? 'fnm-version-list' : 'nvm-version-list'
+        const stdout = await spawnPromise(`bash`, [copyfile, flag, global.Server.Password, 'null', 'ls-remote'])
+        const str = stdout ?? ''
+        const all =
+          str?.match(/\sv\d+(\.\d+){1,4}\s/g)?.map((v) => {
+            return v.trim().replace('v', '')
+          }) ?? []
+        resolve({
+          all: all.reverse(),
+          tool
+        })
       } catch (e) {
         reject(e)
       }
@@ -48,52 +51,40 @@ class Manager extends Base {
   }
 
   localVersion(tool: 'fnm' | 'nvm') {
-    return new ForkPromise((resolve, reject) => {
-      let command = ''
-      if (tool === 'fnm') {
-        command = 'fnm ls'
-      } else {
-        command =
-          'export NVM_DIR="${HOME}/.nvm";[ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh";nvm ls'
-      }
+    return new ForkPromise(async (resolve, reject) => {
       try {
-        exec(
-          command,
-          {
-            env: fixEnv()
-          },
-          (error, stdout) => {
-            let localVersions: Array<string> = []
-            let current = ''
-            if (tool === 'fnm') {
-              localVersions = stdout.match(/\d+(\.\d+){1,4}/g) ?? []
-              const regex = /(\d+(\.\d+){1,4}) default/g
-              const arr = regex.exec(stdout)
-              if (arr && arr.length > 1) {
-                current = arr[1]
-              }
-            } else {
-              const str = stdout
-              const ls = str.split('default')[0]
-              localVersions = ls.match(/\d+(\.\d+){1,4}/g) ?? []
-              const reg = /default.*?(\d+(\.\d+){1,4}).*?\(/g
-              const currentArr: any = reg.exec(str)
-              if (currentArr?.length > 1) {
-                current = currentArr[1]
-              } else {
-                current = ''
-              }
-            }
-            localVersions?.sort((a, b) => {
-              return compareVersions(b, a)
-            })
-            resolve({
-              versions: localVersions,
-              current: current,
-              tool
-            })
+        const copyfile = await this._init_sh()
+        const flag = tool === 'fnm' ? 'fnm-version-list' : 'nvm-version-list'
+        const stdout = await spawnPromise(`bash`, [copyfile, flag, global.Server.Password, 'null', 'ls'])
+        let localVersions: Array<string> = []
+        let current = ''
+        if (tool === 'fnm') {
+          localVersions = stdout.match(/\d+(\.\d+){1,4}/g) ?? []
+          const regex = /(\d+(\.\d+){1,4}) default/g
+          const arr = regex.exec(stdout)
+          if (arr && arr.length > 1) {
+            current = arr[1]
           }
-        )
+        } else {
+          const str = stdout
+          const ls = str.split('default')[0]
+          localVersions = ls.match(/\d+(\.\d+){1,4}/g) ?? []
+          const reg = /default.*?(\d+(\.\d+){1,4}).*?\(/g
+          const currentArr: any = reg.exec(str)
+          if (currentArr?.length > 1) {
+            current = currentArr[1]
+          } else {
+            current = ''
+          }
+        }
+        localVersions?.sort((a, b) => {
+          return compareVersions(b, a)
+        })
+        resolve({
+          versions: localVersions,
+          current: current,
+          tool
+        })
       } catch (e) {
         reject(e)
       }
@@ -101,28 +92,17 @@ class Manager extends Base {
   }
 
   versionChange(tool: 'fnm' | 'nvm', select: string) {
-    return new ForkPromise((resolve, reject) => {
-      let command = ''
-      if (tool === 'fnm') {
-        command = `fnm default ${select}`
-      } else {
-        command = `export NVM_DIR="\${HOME}/.nvm";[ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh";nvm alias default ${select}`
-      }
+    return new ForkPromise(async (resolve, reject) => {
       try {
-        exec(
-          command,
-          {
-            env: fixEnv()
-          },
-          async () => {
-            const { current }: any = await this.localVersion(tool)
-            if (current === select) {
-              resolve(true)
-            } else {
-              reject(new Error('Fail'))
-            }
-          }
-        )
+        const copyfile = await this._init_sh()
+        const flag = tool === 'fnm' ? 'fnm-default-version-change' : 'nvm-default-version-change'
+        await spawnPromise(`bash`, [copyfile, flag, global.Server.Password, select, 'null'])
+        const { current }: any = await this.localVersion(tool)
+        if (current === select) {
+          resolve(true)
+        } else {
+          reject(new Error('Fail'))
+        }
       } catch (e) {
         reject(e)
       }
@@ -157,35 +137,25 @@ class Manager extends Base {
   }
 
   installOrUninstall(tool: 'fnm' | 'nvm', action: 'install' | 'uninstall', version: string) {
-    return new ForkPromise((resolve, reject) => {
-      let command = ''
-      if (tool === 'fnm') {
-        command = `fnm ${action} ${version}`
-      } else {
-        command = `export NVM_DIR="\${HOME}/.nvm";[ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh";nvm ${action} ${version}`
-      }
+    return new ForkPromise(async (resolve, reject) => {
       try {
-        exec(
-          command,
-          {
-            env: fixEnv()
-          },
-          async () => {
-            const { versions, current }: { versions: Array<string>; current: string } =
-              (await this.localVersion(tool)) as any
-            if (
-              (action === 'install' && versions.includes(version)) ||
-              (action === 'uninstall' && !versions.includes(version))
-            ) {
-              resolve({
-                versions,
-                current
-              })
-            } else {
-              reject(new Error('Fail'))
-            }
-          }
-        )
+        const copyfile = await this._init_sh()
+        const flag = tool === 'fnm' ? 'fnm-install-version' : 'nvm-install-version'
+        await spawnPromise(`bash`, [copyfile, flag, global.Server.Password, version, action])
+
+        const { versions, current }: { versions: Array<string>; current: string } =
+          (await this.localVersion(tool)) as any
+        if (
+          (action === 'install' && versions.includes(version)) ||
+          (action === 'uninstall' && !versions.includes(version))
+        ) {
+          resolve({
+            versions,
+            current
+          })
+        } else {
+          reject(new Error('Fail'))
+        }
       } catch (e) {
         reject(e)
       }
@@ -195,21 +165,10 @@ class Manager extends Base {
   nvmDir() {
     return new ForkPromise(async (resolve, reject) => {
       try {
-        const sh = join(global.Server.Static!, 'sh/node.sh')
-        const copyfile = join(global.Server.Cache!, 'node.sh')
-        if (existsSync(copyfile)) {
-          await unlink(copyfile)
-        }
-        await copyFile(sh, copyfile)
-        await execPromise(`echo "${global.Server.Password}" | sudo -S chmod 777 ${copyfile}`)
+        const copyfile = await this._init_sh()
         const stdout = await spawnPromise(`bash`, [copyfile, 'check'])
-        await appendFile(
-          join(global.Server.BaseDir!, 'debug.log'),
-          `[Node][nvmDir][Info]: stdout: ${stdout}\n${JSON.stringify(process.env, undefined, 4)}`
-        )
         resolve(stdout.trim())
       } catch (e) {
-        await appendFile(join(global.Server.BaseDir!, 'debug.log'), `[Node][nvmDir][Error]: ${e}`)
         reject(e)
       }
     })
