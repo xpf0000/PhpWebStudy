0f5bacdc876edaa86423d971062e1afa4da08325
xupengfei <250881478@qq.com>
2024-07-30 10:59:27 +0800

    init

--- src/render/App.vue
+++ src/render/App.vue
@@ -64,6 +64,7 @@ const showAbout = () => {
   Base.Dialog(import('./components/About/index.vue'))
     .className('about-dialog')
     .title(I18nT('base.about'))
+    .width('665px')
     .noFooter()
     .show()
 }
