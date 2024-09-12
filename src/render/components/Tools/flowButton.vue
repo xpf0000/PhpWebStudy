ecfbb32b43003622ec245ae1501ee3195a457876
徐鹏飞 <250881478@qq.com>
2024-09-12 17:49:03 +0800

    1. All Service Add 'add to path' now 2. Add Composer Manager 3. Add Java JDK Manager 4. Add Tomcar Manager

---
+++ src/render/components/Tools/flowButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-ai-btn" @click.stop="showChat">
+    <yb-icon :svg="import('@/svg/ai.svg?raw')" width="26" height="26" />
+  </div>
+  <Chat ref="chat" />
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+  import Chat from './Chat/index.vue'
+  import { AIStore } from '@/components/AI/store'
+  import { I18nT } from '@shared/lang'
+
+  const aiStore = AIStore()
+  const chat = ref()
+  const showChat = () => {
+    if (aiStore.chatList.length === 0) {
+      aiStore.chatList.push({
+        user: 'ai',
+        content: I18nT('ai.我是pipi')
+      })
+    }
+    chat.value.show()
+  }
+</script>
