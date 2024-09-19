bbeaedb121eaabf02c90ad987ddfe358b393f517
徐鹏飞 <250881478@qq.com>
2024-09-19 10:42:36 +0800

    1. Fixed Issues

---
+++ configs/publish.ts
@@ -0,0 +1,9 @@
+import type { GithubOptions } from 'builder-util-runtime'
+
+const conf: GithubOptions = {
+  provider: 'github',
+  owner: 'xpf0000',
+  repo: 'PhpWebStudy'
+}
+
+export default conf
